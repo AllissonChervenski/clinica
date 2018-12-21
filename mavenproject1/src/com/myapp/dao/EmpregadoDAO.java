1f26d9960e326bbd8ee42356b206b26b5cbef40a
Allisson <allissonchervenski5@gmail.com>
2018-12-21 16:54:45 -0200

    VAMO MERDA

--- mavenproject1/src/com/myapp/dao/EmpregadoDAO.java
+++ mavenproject1/src/com/myapp/dao/EmpregadoDAO.java
@@ -1,85 +1,23 @@
 package com.myapp.dao;
 
+import com.boraji.tutorial.hibernate.JPAUtil;
 import com.myapp.model.Empregado;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.EntityManager;
 
 public class EmpregadoDAO {
-
-    public boolean insertEmpregado(Empregado item) {
-        String sql = "INSERT INTO `empregado` (`id_pessoa`, `salarioBase`) VALUES(1, ? )";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
+public boolean insertEmpregado(Empregado empregado)
+    {
         try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setFloat(1, item.getSalarioBase());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
+            EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+            entityManager.getTransaction().begin();
+            entityManager.persist(empregado);
+            entityManager.getTransaction().commit();
+
             return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-    }
-
-    public List<String[]> selectAll() {
-        String sql = "SELECT * FROM empregado";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        Statement stmt = null;
-
-        List<String[]> listDados = new ArrayList();
-
-        try {
-            stmt = conexao.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                String[] dados = new String[1];
-                dados[0] = rs.getString("salarioBase");
-                listDados.add(dados);
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.toString());
-            return null;
         } catch (Exception ex) {
-            System.out.println(ex.toString());
-            return null;
-        }
-        return listDados;
-    }
+            ex.printStackTrace();
 
-    public boolean updateEmpregado(Empregado item) {
-        String sql = "UPDATE `empregado` SET `salarioBase` = ? WHERE `empregado`.`id` = ? ";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setFloat(1, item.getSalarioBase());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-    }
-
-    public boolean deleteEmpregado(Empregado item) {
-        String sql = "DELETE FROM `empregado` WHERE `empregado`.`id` = ? ";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setInt(1, item.getId());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
             return false;
         }
     }
-
 }
--- mavenproject1/src/com/myapp/dao/EnderecoDAO.java
+++ mavenproject1/src/com/myapp/dao/EnderecoDAO.java
@@ -1,91 +1,22 @@
 package com.myapp.dao;
 
+import com.boraji.tutorial.hibernate.JPAUtil;
 import com.myapp.model.Endereco;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.EntityManager;
 
 public class EnderecoDAO {
 
-    public boolean insertEndereco(Endereco item) {
-        String sql = "INSERT INTO `endereco` (`endereco`) VALUES( ? )";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
+    public boolean insertEndereco(Endereco endereco) {
         try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setString(1, item.getDescricao());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
+            EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+            entityManager.getTransaction().begin();
+            entityManager.persist(endereco);
+            entityManager.getTransaction().commit();
+
             return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-    }
-
-    public List<String[]> selectAll() {
-        String sql = "SELECT * FROM endereco";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        Statement stmt = null;
-
-        List<String[]> listDados = new ArrayList();
-
-        try {
-            stmt = conexao.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                String[] dados = new String[5];
-                dados[0] = rs.getString("rua");
-                dados[1] = rs.getString("bairro");
-                dados[2] = rs.getString("uf");
-                dados[3] = rs.getString("numero");
-                dados[4] = rs.getString("cidade");
-                listDados.add(dados);
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.toString());
-            return null;
         } catch (Exception ex) {
-            System.out.println(ex.toString());
-            return null;
-        }
-        return listDados;
-    }
+            ex.printStackTrace();
 
-    public boolean updateEndereco(Endereco item) {
-        String sql = "UPDATE `endereco` SET `rua` = ? , `bairro` = ? , `uf` = ? , `numero` = ? , `cidade` = ? WHERE `endereco`.`id` = ? ";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setString(1, item.getRua());
-            ps.setString(2, item.getBairro());
-            ps.setString(3, item.getUf());
-            ps.setString(4, item.getCidade());
-            ps.setInt(5, item.getNumero());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-    }
-
-    public boolean deleteEndereco(Endereco item) {
-        String sql = "DELETE FROM `endereco` WHERE `endereco`.`id` = ? ";
-        java.sql.Connection conexao = ConnectDataBase.getConexao();
-        try {
-            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setInt(1, item.getId());
-            ps.execute();
-            ConnectDataBase.fecharConexao();
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
             return false;
         }
     }
