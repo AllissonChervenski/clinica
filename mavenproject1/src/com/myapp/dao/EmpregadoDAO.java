eb040d4572878d0cc70b46f176d8a634108a1268
Allisson <allissonchervenski5@gmail.com>
2018-12-21 17:17:29 -0200

    EmpregadoDAO

--- mavenproject1/src/com/myapp/dao/EmpregadoDAO.java
+++ mavenproject1/src/com/myapp/dao/EmpregadoDAO.java
@@ -2,22 +2,45 @@ package com.myapp.dao;
 
 import com.boraji.tutorial.hibernate.JPAUtil;
 import com.myapp.model.Empregado;
+import java.util.Collection;
 import javax.persistence.EntityManager;
 
 public class EmpregadoDAO {
-public boolean insertEmpregado(Empregado empregado)
-    {
+
+    public boolean insertEmpregado(Empregado empregado) {
         try {
             EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+            
             entityManager.getTransaction().begin();
             entityManager.persist(empregado);
             entityManager.getTransaction().commit();
-
             return true;
         } catch (Exception ex) {
             ex.printStackTrace();
-
             return false;
         }
+    }
+
+    public boolean deleteEmpregado(Empregado empregado) {
+        try {
+            EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+            entityManager.getTransaction().begin();
+            empregado = entityManager.find(Empregado.class, empregado.getId());
+            entityManager.remove(empregado);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    public Collection<Empregado> selectAll() {
+        String oql = "From " + Empregado.class.getName() + "";
+        Collection<Empregado> listDados = null;
+        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+        listDados = entityManager.createQuery(oql).getResultList();
+        entityManager.close();
+        return listDados;
     }
 }
