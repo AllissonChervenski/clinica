c3b1658a19a4271dc3bb100ff6f924474a6dbe49
Mauricio Escobar <mauriciosescobar@gmail.com>
2018-12-21 17:12:44 -0200

    prontuarioDAO com CRUD

--- mavenproject1/src/com/myapp/dao/ProntuarioDAO.java
+++ mavenproject1/src/com/myapp/dao/ProntuarioDAO.java
@@ -13,98 +13,65 @@ import javax.persistence.Persistence;
 
 public class ProntuarioDAO
 {
-        public boolean insertProntuario(Prontuario prontuario)
-        {
-//        String sql = "INSERT INTO `prontuario` (`id_paciente`, `id_medico`, `avaliacao`, `subjetivo`, `objetivo`) VALUES(1, 1, ?, ?, ? )";
-//        java.sql.Connection conexao = ConnectDataBase.getConexao();
-//        try {
-//            java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-//            ps.setString(1, item.getAvaliacao());
-//            ps.setString(2, item.getObjetivo());
-//            ps.setString(3, item.getSubjetivo());
-//            ps.execute();
-//            ConnectDataBase.fecharConexao();
-//            return true;
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
-            try {
-                EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
-                entityManager.getTransaction().begin();
-                entityManager.persist(prontuario);
-                entityManager.getTransaction().commit();
-                
-                return true;
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                
-                return false;
-            }
+
+    EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+
+    public boolean insertProntuario(Prontuario prontuario)
+    {
+        try {
+
+            entityManager.getTransaction().begin();
+            entityManager.persist(prontuario);
+            entityManager.getTransaction().commit();
+
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            return false;
         }
+    }
 
-        public List<String[]> selectAll()
-        {
-            String sql = "SELECT * FROM prontuario";
-            java.sql.Connection conexao = ConnectDataBase.getConexao();
-            Statement stmt = null;
+    @SuppressWarnings("unchecked")
+    public List<Prontuario> findAll()
+    {
+        return entityManager.createQuery("FROM " + Prontuario.class.getName()).getResultList();
+    }
 
-            List<String[]> listDados = new ArrayList();
+    public boolean updateProntuario(Prontuario prontuario)
+    {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(prontuario);
+            entityManager.getTransaction().commit();
 
-            try {
-                stmt = conexao.createStatement();
-                ResultSet rs = stmt.executeQuery(sql);
+            return true;
 
-                while (rs.next()) {
-                    String[] dados = new String[3];
-                    dados[0] = rs.getString("avaliacao");
-                    dados[1] = rs.getString("objetivo");
-                    dados[2] = rs.getString("subjetivo");
-                    listDados.add(dados);
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.toString());
-                return null;
-            } catch (Exception ex) {
-                System.out.println(ex.toString());
-                return null;
-            }
-            return listDados;
-        }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
 
-        public boolean updateProntuario(Prontuario item)
-        {
-            String sql = "UPDATE `prontuario` SET `avaliacao` = ?, `objetivo` = ?, `subjetivo` = ? WHERE `prontuario`.`id` = ? ";
-            java.sql.Connection conexao = ConnectDataBase.getConexao();
-            try {
-                java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-                ps.setString(1, item.getAvaliacao());
-                ps.setString(2, item.getObjetivo());
-                ps.setString(3, item.getSubjetivo());
-                ps.execute();
-                ConnectDataBase.fecharConexao();
-                return true;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return false;
-            }
-
-        }
-
-        public boolean deleteProntuario(Prontuario item)
-        {
-            String sql = "DELETE FROM `prontuario` WHERE `prontuario`.`id` = ? ";
-            java.sql.Connection conexao = ConnectDataBase.getConexao();
-            try {
-                java.sql.PreparedStatement ps = conexao.prepareStatement(sql);
-                ps.setInt(1, item.getId());
-                ps.execute();
-                ConnectDataBase.fecharConexao();
-                return true;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return false;
-            }
+            return false;
         }
 
     }
+
+    public boolean deleteProntuario(Prontuario prontuario)
+    {
+        try {
+            entityManager.getTransaction().begin();
+            prontuario = entityManager.find(Prontuario.class, prontuario.getId());
+            entityManager.remove(prontuario);
+            entityManager.getTransaction().commit();
+
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            entityManager.getTransaction().rollback();
+
+            return false;
+        }
+    }
+
+}
